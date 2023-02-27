cd88f913050e39b1c4161dc1cf9bae8d788abcd2
SfisoGentz <sfisogentz02.ramahlo@gmail.com>
2023-02-27 14:03:04 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_strlen - returns the length of string
+*@s: char to check
+*Return: 0;
+*/
+
+int _strlen(char *s)
+{
+	int a = 0; /* start count from 0*/
+
+	for (; *s++;)
+		a++;
+	return (a);
+}
