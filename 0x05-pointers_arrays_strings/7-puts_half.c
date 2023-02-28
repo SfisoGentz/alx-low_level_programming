75c4d9cfa00d95cfedd82d7b02533cf7f97a5269
SfisoGentz <sfisogentz02.ramahlo@gmail.com>
2023-02-27 19:55:07 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* puts_half - prints half of the screen
+*@str: input
+*/
+
+void puts_half(char *str)
+{
+	int leng = 0;
+
+	while (*str != '\0')
+	{
+		leng++;
+		str++;
+	}
+
+	str -= (len / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+
+	_putchar('\n');
+}
