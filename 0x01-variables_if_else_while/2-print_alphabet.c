dd014825a71c577dcf1853842c90692ea9a9ea49
SfisoGentz <sfisogentz02.ramahlo@gmail.com>
2023-09-11 19:03:25 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - this is a function of the task
+ *Return: 0
+ */
+
+int main(void)
+
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	putchar (i);
+
+	putchar ('\n');
+	}
+	return (0);
+}
