99c49fedd537ea672a97a666c7ff29d524bfeb5c
SfisoGentz <sfisogentz02.ramahlo@gmail.com>
2023-09-22 14:37:45 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,9 +7,11 @@
  * Return: if str1 < str2, the negative difference of the first unmatched characters
  * if str1 == str2; 0
  * if str1 > str2, the positive difference of the first unmatched characters
+ *
  */
 
 int _strcmp(char *s1, char *s2)
+
 {
 	while ( *s1 && *s2 && *s1 == *s2)
 	{
