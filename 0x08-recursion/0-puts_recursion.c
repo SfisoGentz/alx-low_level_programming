4c6ba0295c2e8d803ea455ad9e27c09c79e4c3e3
SfisoGentz <sfisogentz02.ramahlo@gmail.com>
2023-09-27 14:21:45 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,12 +3,12 @@
 /**
  * _puts_recursion - Prints scren followed by a new line
  * @s: The string to print
- * Return: nothing
+ * Return: Nothing
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s =='\0')
+	if (*s == '\0')
 	{
 	_putchar('\n');
 	return;
