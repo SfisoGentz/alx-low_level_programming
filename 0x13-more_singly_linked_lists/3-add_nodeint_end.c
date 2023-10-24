42d1afa1515ad7b379fd73471c3ab8d7e7507915
SfisoGentz <sfisogentz02.ramahlo@gmail.com>
2023-10-24 15:45:31 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -5,6 +5,7 @@
  *                   end of the listint_t list
  * @head: A pointer to the address of the 
  *        head of the listint_t list
+ * @n: The integer for the new node to contain
  * Return: If the function fails - NULL
  *         Otherwise - the address of the new element
  */
